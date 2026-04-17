7ed1f1f301c59badec016c1bc53e8aac7de6d001
MagicMayhem117 <mdwyer05@alumnos.uaq.mx>
2026-04-17 17:18:27 -0600

    Otro cambio a los puertos

--- backend/server.js
+++ backend/server.js
@@ -124,6 +124,6 @@ app.delete('/api/businesses/:id', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
